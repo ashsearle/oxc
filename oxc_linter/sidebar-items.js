572dbb14cb08e8ec286daf73033ad485b9c2e323
Boshen <Boshen@users.noreply.github.com>
2023-03-06 14:35:25 +0000

    deploy: d4a10e3fac73dded20fbd5e70da9f1112b00b563

--- oxc_linter/sidebar-items.js
+++ oxc_linter/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["rule",""]],"struct":[["Fixer","The fixer of the code. Note that our parser has handled the BOM, so we don’t need to port the BOM test cases from ESLint."],["Linter",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["rule",""]],"struct":[["Fixer","The fixer of the code. Note that our parser has handled the BOM, so we don’t need to port the BOM test cases from `ESLint`."],["Linter",""]]};

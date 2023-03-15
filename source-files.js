197f8e34b1983c30368e78c7022fe1d361172845
Boshen <Boshen@users.noreply.github.com>
2023-03-15 05:40:18 +0000

    deploy: d1c04523b683a347f2966dba64d85a2e586da615

--- source-files.js
+++ source-files.js
@@ -1,6 +1,6 @@
 var sourcesIndex = JSON.parse('{\
 "oxc_allocator":["",[],["arena.rs","lib.rs"]],\
-"oxc_ast":["",[["ast",[],["js.rs","jsdoc.rs","jsx.rs","literal.rs","mod.rs","operator.rs","ts.rs"]]],["ast_builder.rs","ast_kind.rs","context.rs","lib.rs","serialize.rs","source_type.rs","span.rs","syntax_directed_operations.rs","trivia.rs","visit.rs"]],\
+"oxc_ast":["",[["ast",[],["js.rs","jsdoc.rs","jsx.rs","literal.rs","mod.rs","operator.rs","ts.rs"]]],["ast_builder.rs","ast_kind.rs","context.rs","lib.rs","serialize.rs","source_type.rs","span.rs","syntax_directed_operations.rs","trivia.rs","visit.rs","visit_mut.rs"]],\
 "oxc_benchmark":["",[],["lib.rs"]],\
 "oxc_cli":["",[],["command.rs","lib.rs","options.rs","result.rs","walk.rs"]],\
 "oxc_coverage":["",[],["babel.rs","lib.rs","printer.rs","suite.rs","test262.rs","typescript.rs"]],\

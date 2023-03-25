51ae7c0bcb38dba1789f64b0578ee8167be7fe43
Shannon Rothe <shannon.michael.rothe@gmail.com>
2023-03-26 10:27:25 +1100

    more `@param` parsing + add example

--- crates/oxc_semantic/src/jsdoc/parser.rs
+++ crates/oxc_semantic/src/jsdoc/parser.rs
@@ -1,18 +1,59 @@
 use std::{borrow::Cow, str::FromStr};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum JsDocTagKind {
-    Deprecated,
-    Param,
+pub enum ParamTypeKind {
+    Any,
+    Repeated,
 }
 
-impl FromStr for JsDocTagKind {
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ParamType<'a> {
+    value: &'a str,
+}
+
+impl<'a> ParamType<'a> {
+    #[allow(unused)]
+    pub fn kind(&self) -> Option<ParamTypeKind> {
+        ParamTypeKind::from_str(self.value).map(Option::Some).unwrap_or_default()
+    }
+}
+
+impl FromStr for ParamTypeKind {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // TODO: This might be inaccurate if the type is listed as {....string} or some variant
+        if s.len() > 3 && &s[0..3] == "..." {
+            return Ok(Self::Repeated);
+        }
+
+        if s == "*" {
+            return Ok(Self::Any);
+        }
+
+        Err(())
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Param<'a> {
+    name: &'a str,
+    r#type: Option<ParamType<'a>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum JsDocTagKind<'a> {
+    Deprecated,
+    Param(Param<'a>),
+}
+
+impl<'a> FromStr for JsDocTagKind<'a> {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "deprecated" => Ok(Self::Deprecated),
-            "param" => Ok(Self::Param),
+            "param" => Ok(Self::Param(Param::default())),
             _ => Err(()),
         }
     }
@@ -20,7 +61,7 @@ impl FromStr for JsDocTagKind {
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct JsDocTag<'a> {
-    pub kind: JsDocTagKind,
+    pub kind: JsDocTagKind<'a>,
     pub description: Cow<'a, str>,
 }
 
@@ -44,6 +85,22 @@ impl<'a> JsDocParser<'a> {
 
     pub fn parse(mut self) -> Vec<JsDocTag<'a>> {
         self.parse_comment(self.source_text)
+    }
+
+    fn advance(&mut self) {
+        if self.current < self.source_text.len() {
+            self.current += 1;
+        }
+    }
+
+    fn at(&mut self, c: char) -> bool {
+        let Some(ch) = self.source_text.chars().nth(self.current) else { return false };
+        if ch == c {
+            self.advance();
+            true
+        } else {
+            false
+        }
     }
 
     fn take_until(&mut self, s: &'a str, predicate: fn(char) -> bool) -> &'a str {
@@ -88,11 +145,52 @@ impl<'a> JsDocParser<'a> {
 
     fn parse_tag(&mut self, comment: &'a str) -> Option<JsDocTag<'a>> {
         let tag = self.take_until(comment, |c| c == ' ' || c == '\n');
-        let Ok(kind) = JsDocTagKind::from_str(tag) else { return None };
+        match JsDocTagKind::from_str(tag) {
+            Ok(kind) => match kind {
+                JsDocTagKind::Deprecated => self.parse_deprecated_tag(comment),
+                JsDocTagKind::Param { .. } => self.parse_param_tag(comment),
+            },
+            Err(_) => None,
+        }
+    }
+
+    fn parse_deprecated_tag(&mut self, comment: &'a str) -> Option<JsDocTag<'a>> {
         self.skip_whitespace(comment);
         let description = self.take_until(comment, |c| c == '\n' || c == '*');
+        return Some(JsDocTag {
+            kind: JsDocTagKind::Deprecated,
+            description: Cow::Borrowed(description),
+        });
+    }
 
-        return Some(JsDocTag { kind, description: Cow::Borrowed(description) });
+    fn parse_param_tag(&mut self, comment: &'a str) -> Option<JsDocTag<'a>> {
+        self.skip_whitespace(comment);
+
+        let mut r#type = None;
+
+        if self.at('{') {
+            // If we hit a space, then treat it as the end of the type annotation.
+            let type_annotation = self.take_until(comment, |c| c == '}' || c == ' ');
+            r#type = Some(ParamType { value: type_annotation });
+            if self.at('}') {
+                self.skip_whitespace(comment);
+            }
+            self.skip_whitespace(comment);
+        }
+
+        let name = self.take_until(comment, |c| c == ' ' || c == '\n');
+
+        self.skip_whitespace(comment);
+        if self.at('-') {
+            self.skip_whitespace(comment);
+        }
+
+        let description = self.take_until(comment, |c| c == '\n' || c == '*');
+
+        return Some(JsDocTag {
+            kind: JsDocTagKind::Param(Param { name, r#type }),
+            description: Cow::Borrowed(description),
+        });
     }
 }
 
@@ -101,7 +199,19 @@ mod test {
     use std::borrow::Cow;
 
     use super::JsDocParser;
-    use crate::jsdoc::parser::{JsDocTag, JsDocTagKind};
+    use crate::jsdoc::parser::{JsDocTag, JsDocTagKind, Param, ParamType, ParamTypeKind};
+
+    #[test]
+    fn deduces_correct_param_kind() {
+        let param = Param { name: "a", r#type: Some(ParamType { value: "string" }) };
+        assert_eq!(param.r#type.map(|t| t.kind()).flatten(), None);
+
+        let param = Param { name: "a", r#type: Some(ParamType { value: "...string" }) };
+        assert_eq!(param.r#type.map(|t| t.kind()).flatten(), Some(ParamTypeKind::Repeated));
+
+        let param = Param { name: "a", r#type: Some(ParamType { value: "*" }) };
+        assert_eq!(param.r#type.map(|t| t.kind()).flatten(), Some(ParamTypeKind::Any));
+    }
 
     #[test]
     fn parses_single_line_jsdoc() {
@@ -142,7 +252,10 @@ mod test {
         assert_eq!(
             tags,
             vec![
-                JsDocTag { kind: JsDocTagKind::Param, description: Cow::Borrowed("a") },
+                JsDocTag {
+                    kind: JsDocTagKind::Param(Param { name: "a", r#type: None }),
+                    description: Cow::Borrowed("")
+                },
                 JsDocTag { kind: JsDocTagKind::Deprecated, description: Cow::Borrowed("") },
             ]
         );
@@ -161,10 +274,52 @@ mod test {
         assert_eq!(
             tags,
             vec![
-                JsDocTag { kind: JsDocTagKind::Param, description: Cow::Borrowed("a") },
+                JsDocTag {
+                    kind: JsDocTagKind::Param(Param { name: "a", r#type: None }),
+                    description: Cow::Borrowed("")
+                },
                 JsDocTag {
                     kind: JsDocTagKind::Deprecated,
                     description: Cow::Borrowed("since version 1.0")
+                },
+            ]
+        );
+    }
+
+    #[test]
+    fn parses_param_type_annotation() {
+        let source = r#"/**
+        * @param {string} a
+        * @param {string b
+        * @param {string} c - description
+        */
+       "#;
+
+        let tags = JsDocParser::new(source).parse();
+        assert_eq!(tags.len(), 3);
+        assert_eq!(
+            tags,
+            vec![
+                JsDocTag {
+                    kind: JsDocTagKind::Param(Param {
+                        name: "a",
+                        r#type: Some(ParamType { value: "string" })
+                    }),
+                    description: Cow::Borrowed("")
+                },
+                JsDocTag {
+                    kind: JsDocTagKind::Param(Param {
+                        name: "b",
+                        r#type: Some(ParamType { value: "string" })
+                    }),
+                    description: Cow::Borrowed("")
+                },
+                JsDocTag {
+                    kind: JsDocTagKind::Param(Param {
+                        name: "c",
+                        r#type: Some(ParamType { value: "string" })
+                    }),
+                    description: Cow::Borrowed("description")
                 },
             ]
         );
